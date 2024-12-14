573e15995867af95a722439bd51c4bc8e383cf63
Paul Beck <sparkfox23@mailbox.org>
2024-12-14 17:24:05 -0500

    fix mute key

--- config.def.h
+++ config.def.h
@@ -47,7 +47,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
 #include <X11/XF86keysym.h> /* For multimedia key support */
-#include <X11.keysym.h>
+#include <X11/keysym.h>
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
